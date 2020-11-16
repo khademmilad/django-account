67cb542b252bab4877bd8b28fc9f8df81a0fc1db
khademmilad <khademmilad@users.noreply.github.com>
2020-11-16 17:21:06 +0100

    first url in group app

---
+++ group/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Group
+
+class CreateGroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = [
+            'name',
+            'description'
+        ]
--- group/views.py
+++ group/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import CreateGroupForm
 
-# Create your views here.
+
+def group_index(request):
+    return render(request,'group/index.html')
+
+
+def create_group_form(request):
+    form = CreateGroupForm(request.POST)
+    if request.POST and form.is_valid():
+        form.save()
+    form = CreateGroupForm()
+    dic = {
+        'form' : form
+    }
+    return render(request,"group/create_group.html",dic)
